c242811815a3dbcd8329c236095e46bb50a8a5f4
Dourn4556 <vova.goloveshkin@gmail.com>
2024-05-09 17:16:22 +0300

    last down of ass

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import {defineConfig, devices} from "@playwright/test";
 
 export default defineConfig({
     testDir: "./tests", //'./tests-examples',
-    timeout: 2000,
+    timeout: 5000,
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
