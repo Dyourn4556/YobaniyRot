c5bb427ddee673403d06ff60e3accf445f813663
Dourn4556 <vova.goloveshkin@gmail.com>
2024-05-09 15:47:02 +0300

    last day

--- src/pageObject/locators/ListToDoPageLocators.ts
+++ src/pageObject/locators/ListToDoPageLocators.ts
@@ -1,7 +1,8 @@
 export class ListToDoPageLocators {
   static readonly listToDoPageHeaderXpath = "//h2[@data-testid='welcome']";
   static readonly logoutButtonXpath = "//span[@class='MuiButton-label']";
-  static readonly addNewToDoButtonXpath = "//span[@class='MuiIconButton-label']";
+  static readonly addNewToDoButtonXpath =
+    "//span[@class='MuiIconButton-label']";
 
   static readonly markedLikeDoneToDoCheckBoxXpath =
     "//div[@class='sc-breuTD fkEinw'] //span[@aria-disabled='false'] //span[@class='MuiIconButton-label']";
@@ -15,9 +16,13 @@ export class ListToDoPageLocators {
   static readonly availableToDoXpath = "//div[@data-testid='todo-item']";
   static readonly noAvailableToDo = '//h4[@data-testid="no-todos"]';
 
-  /*Create new ToDo Modal*/
-  static readonly createNewToDoModalHeaderXpath = "//h2[@data-testid='sub-header']";
-  static readonly createNewToDoFieldXpath = "//input[@class='MuiInputBase-input MuiOutlinedInput-input']";
-  static readonly createNewToDoButtonXpath = "//button[@data-testid='submit-newTask']";
-  static readonly goBackToToDoListButtonXpath = "//h2[@class='sc-ivTmOn jYMFSu']";
+  /*Create new  ToDo Modal*/
+  static readonly createNewToDoModalHeaderXpath =
+    "//h2[@data-testid='sub-header']";
+  static readonly createNewToDoFieldXpath =
+    "//input[@class='MuiInputBase-input MuiOutlinedInput-input']";
+  static readonly createNewToDoButtonXpath =
+    "//button[@data-testid='submit-newTask']";
+  static readonly goBackToToDoListButtonXpath =
+    "//h2[@class='sc-ivTmOn jYMFSu']";
 }
